603bf4750e589323aa7099ab623e40b8bb53ccd9
engalyelsayed <engalyelsayed>
2025-11-24 18:24:44 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_taskone/screens/screen1.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Screen1(),
+    ));}
+    /*echo "# taskoneflutter" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/engalyelsayed/taskoneflutter.git
+git push -u origin main  */
---
+++ lib/screens/screen1.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+    
+      child: Scaffold(
+        //drawer: Drawer(child: Container(color: Colors.yellow,width: 150,height: 300,
+        //child:Text ("text drawer"),),
+
+      //),
+        appBar: AppBar(backgroundColor:Colors.blue ,centerTitle: true,
+        leading:
+         IconButton(onPressed: () {  }, icon: 
+          Icon(  Icons.menu,color: Colors.white,)),
+      
+        title: Text("first screen",style: TextStyle(fontSize: 26,
+        color: Colors.white),),
+        actions: [
+          IconButton(onPressed: (){}, icon:Icon(Icons.search,color: Colors.white)),
+          
+          IconButton(onPressed: () {
+            
+          },icon:  Icon(Icons.more_vert,color: Colors.white,)),
+          
+        ],
+        ),
+        body:Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [  SizedBox(height: 10,), Image.network(
+            'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
+            fit: BoxFit.cover,
+          ),SizedBox(height: 15,), Image.network(
+            'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
+            fit: BoxFit.cover,
+          ),SizedBox(height: 45,),
+          Center(child: Text("wellcom all friends ",style: TextStyle(fontSize: 30,fontWeight:FontWeight.bold,color:Colors.red),))]),
+        ),
+
+        
+        
+        
+
+
+          
+             bottomNavigationBar: BottomNavigationBar(backgroundColor: Colors.blue,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home,size:40,color: Colors.white,),label: "home",),
+            BottomNavigationBarItem(icon: Icon(Icons.message,size:40,color: Colors.white,),label: "message"),
+            BottomNavigationBarItem(icon: Icon(Icons.person,size:40,color: Colors.white,),label: "profile"),
+          ],
+
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: (
+          
+        ) {
+        
+          
+        },backgroundColor: Colors.blue,child: Icon(Icons.add,size: 28,
+        color: Colors.white,),
+          ),
+        ),
+        
+          
+      
+    );
+  }}
+  /*
+  echo "# flutterproject" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/engalyelsayed/flutterproject.git
+git push -u origin main*/
+
+
+/*
+git init
+git remote add origin
+git add .
+git commit -m "first commit"
+git branch -M main
+git push -u origin main
+*/
