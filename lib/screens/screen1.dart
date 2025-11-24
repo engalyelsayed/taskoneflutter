fcaabe16ce8f9c76544969e9428961c4d4e86e96
engalyelsayed <91175399+engalyelsayed@users.noreply.github.com>
2025-11-24 20:36:16 +0200

    Clean up screen1.dart by removing comments
    Removed commented out Git commands from screen1.dart

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -69,21 +69,4 @@ class Screen1 extends StatelessWidget {
       
     );
   }}
-  /*
-  echo "# flutterproject" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/engalyelsayed/flutterproject.git
-git push -u origin main*/
 
-
-/*
-git init
-git remote add origin
-git add .
-git commit -m "first commit"
-git branch -M main
-git push -u origin main
-*/
